d319c5dd9867ea3d67f1ba98a563a611dee8e887
HCerimac <168143319+HCerimac@users.noreply.github.com>
2024-05-03 19:15:19 -0300

    Add files via upload

---
+++ calculadora_exercicio.py
@@ -0,0 +1,10 @@
+n1 = float(input('>>'))
+n2 = float(input('>>'))
+soma = n1+n2
+print('resultado da soma : ', soma)
+subtracao = n1-n2
+print('resultado da subtracao : ', subtracao)
+multiplicacao = n1*n2
+print('resultado da multiplicacao : ', multiplicacao)
+divisao = n1/n2
+print('resultado da divisao : ', divisao)
